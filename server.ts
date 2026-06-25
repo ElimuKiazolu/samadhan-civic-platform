a4b1e3aa2469ed04a5dc2fa756c6627f939c0369
Elimu S Kiazolu <kiazoluelimu@gmail.com>
2026-06-25 22:55:23 +0530

    fix: serve static frontend from __dirname (dist path was doubled in container)

--- server.ts
+++ server.ts
@@ -133,7 +133,7 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const distPath = path.join(__dirname, 'dist');
+   const distPath = __dirname;  // const distPath = path.join(__dirname, 'dist');
     app.use(express.static(distPath));
     app.get('*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
